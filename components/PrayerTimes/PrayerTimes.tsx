35a162eb8e7a38740ba871db0742f3892c233900
omrltff <olatif04@gmail.com>
2024-05-02 14:37:20 -0400

    css

--- components/PrayerTimes/PrayerTimes.tsx
+++ components/PrayerTimes/PrayerTimes.tsx
@@ -79,10 +79,10 @@ export default function PrayerTimes({
               border border-mosqueGreen-dark border-l-0 border-r-0
               last-of-type:border-b-0"
           >
-            <th className="text-left text-xl md:text-5xl md:text-right">
+            <th className="text-left text-xl md:text-6xl md:text-right">
               {prayer.label}
             </th>
-            <td className="text-xl md:text-6xl">
+            <td className="text-xl md:text-7xl">
               {moment(prayer.data.start, ["HH:mm"]).format("h:mm")}
               {prayer.data?.start_secondary ? (
                 <div className="block mt-1 md:mt-2">
@@ -92,7 +92,7 @@ export default function PrayerTimes({
                 </div>
               ) : null}
             </td>
-            <td className={`font-bold text-xl md:text-6xl`}>
+            <td className={`font-bold text-xl md:text-7xl`}>
               <span
                 className={
                   nextPrayerTime.today === true &&
@@ -106,7 +106,7 @@ export default function PrayerTimes({
                 )}
               </span>
             </td>
-            <td className={`text-xl md:text-6xl`}>
+            <td className={`text-xl md:text-7xl`}>
               <span
                 className={
                   nextPrayerTime.today === false &&

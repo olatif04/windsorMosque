176b2fb15aa680eb275b5e080ca02b3463974909
omrltff <olatif04@gmail.com>
2024-05-02 16:29:32 -0400

    removed padding from bottom widget

--- components/SunriseJummahTiles/SunriseJummahTiles.tsx
+++ components/SunriseJummahTiles/SunriseJummahTiles.tsx
@@ -7,15 +7,10 @@ export default function JummahTiles({
   jummahTimes: JummahTimes
 }) {
   return (
-    <div>
-      <dl
-        className="grid grid-cols-3 justify-items-stretch text-center gap-0 md:gap-3"
-      >
+    <div className="bg-mosqueGreen-dark text-white">
+      <dl className="grid grid-cols-3 justify-items-stretch text-center gap-0 md:gap-3 p-4 lg:p-6">
         {jummahTimes.map((jummahTime, index) => (
-          <div
-            className="bg-mosqueGreen-dark text-white p-4 lg:p-6"
-            key={index}
-          >
+          <div key={index}>
             <dt className="text-sm lg:text-2xl font-medium">
               {jummahTime.label}
             </dt>
@@ -25,11 +20,11 @@ export default function JummahTiles({
           </div>
         ))}
       </dl>
-      {/* Placeholder text section below the Jummah times */}
-      <div className="flex items-center justify-center text-white p-6 lg:p-6 bg-mosqueGreen-dark mt-4">
-  <p className="text-3xl lg:text-2xl">
-    Isha Iqama will change to 9:45PM tomorrow.
-  </p>
+      {/* Integrated notice section */}
+      <div className="flex items-center justify-center p-4 lg:p-6">
+        <p className="text-3xl lg:text-2xl">
+          Isha Iqama will change to 9:45PM tomorrow.
+        </p>
       </div>
     </div>
   )

9c684fbd407cc8d20608f0ca689778d9bfcd3c79
omrltff <olatif04@gmail.com>
2024-05-02 13:32:33 -0400

    phone text removed

--- components/MosqueMetadata/MosqueMetadata.tsx
+++ components/MosqueMetadata/MosqueMetadata.tsx
@@ -19,8 +19,8 @@ export default function MosqueMetadata({
         <h2 className="mt-3 md:mt-5 font-bold text-2xl md:text-3xl">
           {metadata.name}
         </h2>
-        {/* <p className="mt-3 text-xl mx-5 md:mx-0">{metadata.address}</p> */}
-        {/* <p className="text-xl">{metadata.website}</p> */}
+        <p className="mt-3 text-xl mx-5 md:mx-0">{metadata.address}</p>
+        <p className="text-xl">{metadata.website}</p>
       </div>
     </div>
   )

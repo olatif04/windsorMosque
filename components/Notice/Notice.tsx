ee979f40cf9afbb2864451cab392b3aab737f724
omrltff <olatif04@gmail.com>
2024-05-02 13:28:16 -0400

    fixed eslint errors

--- components/Notice/Notice.tsx
+++ components/Notice/Notice.tsx
@@ -23,15 +23,14 @@ export default function Notice() {
         <p className="italic text-xl md:text-2xl md:max-w-lg hidden md:block">
           Please ensure your mobile phone is silent in the prayer hall.
         </p>
-        <hr>
-        </hr>
+        <hr />
         <p className="italic text-xl md:text-2xl md:max-w-lg hidden md:block">
-        Allah's Messenger (ﷺ) said, "A Muslim is a brother of another Muslim, so he should not oppress him, nor should he hand him over to an oppressor. Whoever fulfilled the needs of his brother, Allah will fulfill his needs.
+          Allah&apos;s Messenger (ﷺ) said, &quot;A Muslim is a brother of another Muslim, so he should not oppress him, nor should he hand him over to an oppressor. Whoever fulfilled the needs of his brother, Allah will fulfill his needs.&quot;
         </p>
         <p className="italic text-xl md:text-2xl md:max-w-lg hidden md:block">
-        Sahih al-Bukhari 2442
+          Sahih al-Bukhari 2442
         </p>
       </div>
     </div>
-  )
+  );
 }

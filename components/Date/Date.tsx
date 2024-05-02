322149649753c3f29b1b918133b59ce68c498c22
omrltff <olatif04@gmail.com>
2024-05-02 17:10:17 -0400

    wrapper for date container

--- components/Date/Date.tsx
+++ components/Date/Date.tsx
@@ -6,10 +6,12 @@ export default function Date() {
   const hijriDate = moment().locale("en").format("iD iMMMM iYYYY")
 
   return (
-    <div className="flex items-center justify-center text-white text-center md:text-left">
-      <p className="font-bold text-3xl md:text-5xl">{englishDate}</p>
-      <span className="mx-3 text-5xl md:text-5xl font-bold">|</span> {/* Bold vertical separator */}
-      <p className="font-bold text-3xl md:text-5xl">{hijriDate}</p>
+    <div className="bg-mosqueGreen-dark p-4 lg:p-6"> {/* Added background wrapper with controlled padding */}
+      <div className="flex items-center justify-center text-white text-center md:text-left">
+        <p className="font-bold text-3xl md:text-5xl">{englishDate}</p>
+        <span className="mx-3 text-5xl md:text-5xl font-bold">|</span> {/* Bold vertical separator */}
+        <p className="font-bold text-3xl md:text-5xl">{hijriDate}</p>
+      </div>
     </div>
   )
 }

013621e93d616d61dbd2994177b8a8af32c09873
omrltff <olatif04@gmail.com>
2024-05-02 00:14:19 -0400

    notice added

--- components/SlidingBanner/SlidingBanner.tsx
+++ components/SlidingBanner/SlidingBanner.tsx
@@ -1,18 +1,27 @@
-"use client"
+import { useEffect, useState } from "react";
 
-import { useEffect, useState } from "react"
+interface SlidingBannerProps {
+  slides: JSX.Element[];
+}
 
-const transitionTime = parseInt(process.env.SLIDE_TRANSITION_TIME ?? "7") // defaults to 7 seconds
+const transitionTime = parseInt(process.env.SLIDE_TRANSITION_TIME ?? "7"); // defaults to 7 seconds
 
-export default function SlidingBanner({ slides }: { slides: any }) {
-  const [currentSlide, setCurrentSlide] = useState(0)
+export default function SlidingBanner({ slides }: SlidingBannerProps) {
+  const [currentSlide, setCurrentSlide] = useState(0);
   useEffect(() => {
     const interval = setInterval(() => {
-      setCurrentSlide(currentSlide === slides.length - 1 ? 0 : currentSlide + 1)
-    }, transitionTime * 1000)
+      setCurrentSlide(currentSlide === slides.length - 1 ? 0 : currentSlide + 1);
+    }, transitionTime * 1000);
 
-    return () => clearInterval(interval)
-  }, [currentSlide, slides])
+    return () => clearInterval(interval);
+  }, [currentSlide, slides]);
 
-  return <>{slides[currentSlide]}</>
+  return (
+    <div className="relative w-full">
+      {slides[currentSlide]}
+      <div className="absolute bottom-0 w-full text-center text-sm font-light p-1">
+        Developed by Omer Latif
+      </div>
+    </div>
+  );
 }

aab54358ebd9d2d5d7ca0210d20599d367406be2
omrltff <olatif04@gmail.com>
2024-05-02 15:55:07 -0400

    added logo back

--- components/MosqueMetadata/MosqueMetadata.tsx
+++ components/MosqueMetadata/MosqueMetadata.tsx
@@ -7,16 +7,16 @@ export default function MosqueMetadata({
 }) {
   return (
     <div className="md:flex text-white text-center md:text-left">
-      <div className="hidden mr-4 flex-shrink-0 self-center">
+      <div className=" mr-4 flex-shrink-0 self-center">
         {/* eslint-disable-next-line @next/next/no-img-element */}
         <img
-          className="hidden m-2 max-w-full lg:max-w-lg max-h-16 mx-auto"
+          className="m-2 max-w-full lg:max-w-lg max-h-12 mx-auto"
           src={metadata.logo_url}
           alt=""
         />
       </div>
       <div>
-        <h2 className="mt-3 md:mt-5 font-bold text-3xl md:text-3xl">
+        <h2 className="mt-3 md:mt-5 font-bold text-2xl md:text-3xl">
           {metadata.name}
         </h2>
         <p className="hidden mt-3 text-xl mx-5 md:mx-0">{metadata.address}</p>

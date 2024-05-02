9b5b5412c8b3e4e11f7bb3c2e92f330e5cbf4f90
omrltff <olatif04@gmail.com>
2024-05-02 13:39:03 -0400

    eslint

--- components/Notice/Notice.tsx
+++ components/Notice/Notice.tsx
@@ -3,12 +3,15 @@ export default function Notice() {
     <div className="flex text-white text-center md:text-left">
       <div>
         <p className="italic text-xl md:text-2xl md:max-w-lg hidden md:block">
-          Allah’s Messenger (ﷺ) said, "A Muslim is a brother of another Muslim, so he should not oppress him, nor should he hand him over to an oppressor. Whoever fulfilled the needs of his brother, Allah will fulfill his needs."
+          Allah&apos;s Messenger (ﷺ) said, &quot;A Muslim is a brother of
+          another Muslim, so he should not oppress him, nor should he hand him
+          over to an oppressor. Whoever fulfilled the needs of his brother,
+          Allah will fulfill his needs.&quot;
         </p>
         <p className="italic text-xl md:text-2xl md:max-w-lg hidden md:block">
           Sahih al-Bukhari 2442
         </p>
       </div>
     </div>
-  );
+  )
 }

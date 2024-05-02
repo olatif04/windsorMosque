12b8d1e0b46f46493ac4e06de0c8970f7eedf52b
omrltff <olatif04@gmail.com>
2024-05-02 15:58:46 -0400

    logo styling

--- components/MosqueMetadata/MosqueMetadata.tsx
+++ components/MosqueMetadata/MosqueMetadata.tsx
@@ -6,20 +6,20 @@ export default function MosqueMetadata({
   metadata: MosqueMetadataType
 }) {
   return (
-    <div className="md:flex text-white text-center md:text-left">
-      <div className=" mr-4 flex-shrink-0 self-center">
+    <div className="flex text-white text-center md:text-left">
+      <div className="flex-shrink-0 self-center">
         {/* eslint-disable-next-line @next/next/no-img-element */}
         <img
-          className="m-2 max-w-full lg:max-w-lg max-h-12 mx-auto"
+          className="m-2 max-w-full lg:max-w-lg max-h-16 mx-auto"
           src={metadata.logo_url}
-          alt=""
+          alt={metadata.name}  // Added alt text for accessibility
         />
       </div>
-      <div>
-        <h2 className="mt-3 md:mt-5 font-bold text-2xl md:text-3xl">
+      <div className="self-center ml-4">  // Added margin-left for spacing
+        <h2 className="font-bold text-2xl md:text-3xl">
           {metadata.name}
         </h2>
-        <p className="hidden mt-3 text-xl mx-5 md:mx-0">{metadata.address}</p>
+        <p className="hidden text-xl mx-5 md:mx-0">{metadata.address}</p>
         <p className="hidden text-xl">{metadata.website}</p>
       </div>
     </div>

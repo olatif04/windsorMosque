3d3b812c343b4b58110c196aa766bba65a0ab153
omrltff <olatif04@gmail.com>
2024-05-02 00:18:23 -0400

    changes reverted

--- components/SlidingBanner/SlidingBanner.tsx
+++ components/SlidingBanner/SlidingBanner.tsx
@@ -1,23 +1,18 @@
-import { useEffect, useState } from "react";
+"use client"
 
-const transitionTime = parseInt(process.env.SLIDE_TRANSITION_TIME ?? "7"); // defaults to 7 seconds
+import { useEffect, useState } from "react"
+
+const transitionTime = parseInt(process.env.SLIDE_TRANSITION_TIME ?? "7") // defaults to 7 seconds
 
 export default function SlidingBanner({ slides }: { slides: any }) {
-  const [currentSlide, setCurrentSlide] = useState(0);
+  const [currentSlide, setCurrentSlide] = useState(0)
   useEffect(() => {
     const interval = setInterval(() => {
-      setCurrentSlide(currentSlide === slides.length - 1 ? 0 : currentSlide + 1);
-    }, transitionTime * 1000);
+      setCurrentSlide(currentSlide === slides.length - 1 ? 0 : currentSlide + 1)
+    }, transitionTime * 1000)
 
-    return () => clearInterval(interval);
-  }, [currentSlide, slides]);
+    return () => clearInterval(interval)
+  }, [currentSlide, slides])
 
-  return (
-    <div>
-      {slides[currentSlide]}
-      <div style={{ textAlign: 'center', marginTop: '10px', fontSize: 'small' }}>
-        Developed by Omer Latif
-      </div>
-    </div>
-  );
+  return <>{slides[currentSlide]}</>
 }

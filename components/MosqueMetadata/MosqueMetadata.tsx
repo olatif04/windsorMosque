c3fb2c108581a30ac70f52cdd48178eb6a259ec9
omrltff <olatif04@gmail.com>
2024-05-02 16:59:24 -0400

    fixed jsx comments

--- components/MosqueMetadata/MosqueMetadata.tsx
+++ components/MosqueMetadata/MosqueMetadata.tsx
@@ -10,15 +10,16 @@ export default function MosqueMetadata({
   return (
     <div className="flex items-center text-white text-center md:text-left mt-0"> {/* Use items-center to align items vertically */}
       <div className="flex-shrink-0">
+        {/* Using Next.js Image component for optimized image loading */}
         <Image
           src={logo}
-          alt={metadata.name} // Proper alt text for accessibility
-          width={100} // Adjusted for visual balance
-          height={100} // Adjusted for visual balance
-          layout='responsive' // This will maintain the aspect ratio
+          alt={metadata.name}  // Proper alt text for accessibility
+          width={100}  // Adjusted for visual balance
+          height={100}  // Adjusted for visual balance
+          layout='responsive'  // This will maintain the aspect ratio
         />
       </div>
-      <div className="ml-4" style={{ marginTop: '0.5em' }}>  // Subtle adjustment to lower the text
+      <div className="ml-4" style={{ marginTop: '0.5em' }}> {/* Subtle adjustment to lower the text */}
         <h2 className="font-bold text-4xl md:text-4xl">{metadata.name}</h2> {/* Increased font size */}
         {/* Address and website are hidden; they are included for potential future use but not displayed */}
         <p className="hidden text-xl mx-5 md:mx-0">{metadata.address}</p>

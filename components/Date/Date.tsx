cc057b10c723f10d6031bdaeb152f956136a53b6
omrltff <olatif04@gmail.com>
2024-05-02 17:17:43 -0400

    less margins

--- components/Date/Date.tsx
+++ components/Date/Date.tsx
@@ -6,11 +6,11 @@ export default function Date() {
   const hijriDate = moment().locale("en").format("iD iMMMM iYYYY")
 
   return (
-    <div className="bg-mosqueGreen-dark p-4 lg:p-6"> {/* Added background wrapper with controlled padding */}
-      <div className="flex items-center justify-center text-white text-center md:text-left">
-        <p className="font-bold text-3xl md:text-5xl">{englishDate}</p>
+    <div className="bg-mosqueGreen-dark"> {/* Remove padding here if it creates external space */}
+      <div className="flex items-center justify-center text-white text-center md:text-left p-4 lg:p-6"> {/* Move padding inside */}
+        <p className="font-bold text-3xl md:text-5xl m-0">{englishDate}</p> {/* Remove any margin that might add space */}
         <span className="mx-3 text-5xl md:text-5xl font-bold">|</span> {/* Bold vertical separator */}
-        <p className="font-bold text-3xl md:text-5xl">{hijriDate}</p>
+        <p className="font-bold text-3xl md:text-5xl m-0">{hijriDate}</p> {/* Remove any margin that might add space */}
       </div>
     </div>
   )

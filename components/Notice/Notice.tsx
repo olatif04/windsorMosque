cedf3084bb2cbd45613b41c1ebc86cad92434cab
omrltff <olatif04@gmail.com>
2024-05-02 13:36:25 -0400

    bigger font

--- components/Notice/Notice.tsx
+++ components/Notice/Notice.tsx
@@ -2,10 +2,10 @@ export default function Notice() {
   return (
     <div className="flex text-white text-center md:text-left">
       <div>
-        <p className="italic text-lg md:text-xl md:max-w-lg hidden md:block">
-          Allah&apos;s Messenger (ﷺ) said, &quot;A Muslim is a brother of another Muslim, so he should not oppress him, nor should he hand him over to an oppressor. Whoever fulfilled the needs of his brother, Allah will fulfill his needs.&quot;
+        <p className="italic text-xl md:text-2xl md:max-w-lg hidden md:block">
+          Allah’s Messenger (ﷺ) said, "A Muslim is a brother of another Muslim, so he should not oppress him, nor should he hand him over to an oppressor. Whoever fulfilled the needs of his brother, Allah will fulfill his needs."
         </p>
-        <p className="italic text-lg md:text-xl md:max-w-lg hidden md:block">
+        <p className="italic text-xl md:text-2xl md:max-w-lg hidden md:block">
           Sahih al-Bukhari 2442
         </p>
       </div>
